40a45e1393c8f70a9d341a9e44a2716376e771be
Upendra12395 <ch95upendra@gmail.com>
2021-11-02 16:07:27 +0530

    app file with database connection

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+
+const PORT = process.env.PORT || 4000
+const password = process.env.PASSWORD
+const MONGO_URL = `mongodb+srv://upendraa:${password}@cluster0.mwuaz.mongodb.net/mytodo3?retryWrites=true&w=majority`
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser : true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on('open', ()=>{
+    console.log('Database connected')
+})
+app.listen(PORT, ()=>{
+    console.log(`app is running on ${PORT}`)
+})
